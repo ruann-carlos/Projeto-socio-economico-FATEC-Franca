9bda4a69ff40ba9683c91059074d755aa2a737a5
kefflen <55823300+kefflen@users.noreply.github.com>
2020-09-16 21:46:51 -0300

    ExtairDados

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 
+function extrairDados(vetor) {
+    let finalArray = []
+    for (obj of vetor) {
+        for (prop in obj) {
+            finalArray.push(obj[prop])
+        }
+    }
+    return finalArray
+}
 const url = "/dados/result.json"
 fetch(url)
     .then(resp => resp.json())
     .then(dados => {
-        console.log(dados)
-        console.log(dados[0])
+        let datas = dados.map(pessoa => pessoa[8])
+
+        let dataString = extrairDados(datas)
+        console.log(dataString)
     })
