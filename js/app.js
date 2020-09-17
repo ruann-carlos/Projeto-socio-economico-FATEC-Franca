ca85deae0322b34891a8a48415969a762fa65a1e
ruann_carlos <ruann.carlos@indb.com.br>
2020-09-17 14:08:10 -0300

    obtendo idade final dos usuarios

--- js/app.js
+++ js/app.js
@@ -3,6 +3,25 @@ const url = "/dados/result.json"
 fetch(url)
     .then(resp => resp.json())
     .then(dados => {
-        console.log(dados)
-        console.log(dados[0])
+        let datas = dados.map(pessoa => pessoa[8]);
+        let datasString = getStringAno(datas);
+        idades = datasString.map(dataString => getIdadeByString(dataString))
+        console.log(idades);
     })
+function getStringAno(arr){
+    const arrayAnos= [];
+    for(obj of arr){
+        for(ano in obj){
+            arrayAnos.push(obj[ano]);
+        }
+    }
+    return arrayAnos;
+}
+function getIdadeByString(nascString) {
+    let nascArray = nascString.split("-")
+    let [ano, mes] = [Number(nascArray[0]), Number(nascArray[1])]
+    let idade = 2020 - ano
+    if (mes < 9) {
+        return idade - 1
+    } else return idade
+}
